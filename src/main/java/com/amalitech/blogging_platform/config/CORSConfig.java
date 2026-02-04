7b1aff43a077cb62eb2351769fccce98f5ba9fd6
Ange A. Buhendwa <mickaelbhd@gmail.com>
2026-02-04 21:23:42 +0200

    feat: add CORS configuration

---
+++ src/main/java/com/amalitech/blogging_platform/config/CORSConfig.java
@@ -0,0 +1,30 @@
+package com.amalitech.blogging_platform.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@EnableConfigurationProperties(CORSProperties.class)
+@Configuration
+public class CORSConfig {
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource(CORSProperties props) {
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowedOrigins(props.getAllowedOrigins());
+    config.setAllowedMethods(props.getAllowedMethods());
+    config.setAllowedHeaders(props.getAllowedHeaders());
+    config.setAllowCredentials(props.isAllowCredentials());
+
+    // Cache preflight responses
+    config.setMaxAge(3600L);
+
+    UrlBasedCorsConfigurationSource source =
+            new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+    return source;
+  }
+}
---
+++ src/main/java/com/amalitech/blogging_platform/config/CORSProperties.java
@@ -0,0 +1,17 @@
+package com.amalitech.blogging_platform.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+
+@ConfigurationProperties(prefix = "app.cors")
+@Getter
+@Setter
+public class CORSProperties {
+  private List<String> allowedOrigins;
+  private List<String> allowedMethods;
+  private List<String> allowedHeaders;
+  private boolean allowCredentials;
+}
